bf4d71b7810a46aa3bba5e64a0b25ef1d1dcf58f
Itay <naveh.itay@gmail.com>
2021-10-11 20:01:42 +0300

    copy DriveClass from last year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/Location.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.freight_frenzy.util;
+
+public class Location {
+
+    public double x;
+    public double y;
+
+    public Location(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Location offset(double offset) {
+        return new Location(this.x,this.y + offset);
+    }
+}
