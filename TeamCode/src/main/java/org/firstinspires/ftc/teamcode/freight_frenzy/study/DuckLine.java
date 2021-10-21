793b52be62eee9498fc1e20bd8a1dcca28a27a41
Itay <naveh.itay@gmail.com>
2021-10-21 21:19:50 +0300

    add DuckLine with tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/study/DuckLine.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.freight_frenzy.study;
+
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.core.Mat;
+
+public class DuckLine extends OpenCvPipeline {
+	@Override
+	public Mat processFrame(Mat input) {
+		return input;
+	}
+}
