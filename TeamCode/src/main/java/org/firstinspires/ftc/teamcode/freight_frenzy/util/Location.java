d12297cb51de3a9ed870294f6939c28cb0776723
Itamar-Hadar <itamarush.hadar@gmail.com>
2021-10-21 20:32:32 +0300

    improve (dummy commit)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/Location.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/Location.java
@@ -2,15 +2,15 @@ package org.firstinspires.ftc.teamcode.freight_frenzy.util;
 
 public class Location {
 
-    public double x;
-    public double y;
+	public double x;
+	public double y;
 
-    public Location(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+	public Location(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    public Location offset(double offset) {
-        return new Location(this.x,this.y + offset);
-    }
+	public Location offset(double offset) {
+		return new Location(this.x, this.y + offset);
+	}
 }
