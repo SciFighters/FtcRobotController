c623b853b910e3c3245adb424d769d6cb38a1b7e
liamh <liamhecht800@gmail.com>
2021-10-25 20:00:41 +0300

    #release() function -> stop() #release() function = dispensing objects #added rail_move #added resetpos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/HandRailClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/HandRailClass.java
@@ -7,15 +7,20 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
+// Handrail class
 public class HandRailClass {
+    //motors, servos and touch switch
     private DcMotorEx rail = null;
     private DcMotorEx hand = null;
     private CRServo grabber_right = null;
     private CRServo grabber_left = null;
-    private DigitalChannel grabber_switch = null;
-    private DigitalChannel hand_limit_front = null;
-    private DigitalChannel hand_limit_back = null;
+    private DigitalChannel grabber_switch = null; //???
+    private DigitalChannel hand_limit = null;
+    private DigitalChannel rail_limit = null;
+
+
+
+
     private LinearOpMode opMode;
 
     public HandRailClass(LinearOpMode opMode) {
@@ -30,18 +35,52 @@ public class HandRailClass {
         grabber_left = hw.get(CRServo.class, "grabber_left");
 
         grabber_switch = hw.get(DigitalChannel.class, "grabber_switch");
-        hand_limit_front = hw.get(DigitalChannel.class, "hand_limit_front");
-        hand_limit_back = hw.get(DigitalChannel.class, "hand_limit_back");
+        hand_limit = hw.get(DigitalChannel.class, "hand_limit_front");
+        rail_limit = hw.get(DigitalChannel.class, "hand_limit_back");
+    }
 
+    public void setServosPower(double power) {
+        grabber_left.setPower(power);
+        grabber_right.setPower(power);
+    }
+
+    public double clamp(double value, double maximum, double minimum) {
+        if(maximum < value) value = maximum;
+        if(minimum > value) value = minimum;
+        return value;
     }
 
     public void grab() {
+        setServosPower(0.95);
+    }
 
+    public void stop() {
+        setServosPower(0.0);
+    }
+
+    public void release() {
+        setServosPower(-0.2);
     }
 
     public void rail_move(double pow) {
-       rail.setPower(pow);
+       rail.setPower(1.0);
+       rail.setTargetPosition(500);
     }
+
+
+    public void resetPos(int pos) {
+        rail.setPower(0.0);
+
+    }
+
+    public void touch_switch_update() {
+        if(rail_limit.getState()) {
+            //if touch switch is pressed
+            rail.setPower(0.0);
+            stop();
+        }
+    }
+
 }
 
 
