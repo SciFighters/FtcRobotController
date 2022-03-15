47538600a47e0ca8a5ad4796f5504872078ff9c0
Itamar-Hadar <itamarush.hadar@gmail.com>
2022-03-15 18:53:32 +0200

    Final version before competition lets gooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/autonomous/AutoFlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freight_frenzy/util/autonomous/AutoFlow.java
@@ -79,9 +79,9 @@ public class AutoFlow {
 	Location freightLocation_Pre2 = new Location(-0.60, 0.65, -90); //previously -0.60, 0.85
 	Location freightLocation_Pre3 = new Location(0.15, 0.55, -90);
 	Location freightLocation = new Location(-1.40, 0.61, -90); // -1.5, 0.93
-	Location freightPickup = new Location(-1.3, 0.14, -90);
 	Location freightSideLocation = new Location(-0.6, 0.1, -90);
-	private final Location pre_cycle = new Location(-0.5, 0.14, -90);
+	Location freightPickup = new Location(-1.2, 0.12, -90);
+	Location pre_cycle = new Location(-0.5, 0.12, -90);
 	Location pre_fullPickup = new Location(freightLocation, 0.2, 0.2, -90);
 
 	// Storage locations
@@ -284,20 +284,26 @@ public class AutoFlow {
 		opMode.telemetry.addLine("cycle");
 		opMode.telemetry.update();
 		handrail.gotoRail(100, 1);
-		drive.goToLocation(pre_cycle, 1, 0.05, 0); // first location - pre-barrier
+		drive.goToLocation(pre_cycle, 1, 0.04, 0); // first location - pre-barrier
 		//drive.turnTo(pre_cycle.angle, 0.5);
 		handrail.gotoLevel(DuckLine.SH_Levels.CollectAuto);
 		handrail.grabberGrab();
 		//TOUCH SWITCH CHECKER FAZE
-		double firstRoundX = -1.3;
+		double firstRoundX = -1.1;
 		double reachTheFreightCalc = (firstRoundX - round * 0.05) * alliance.mul;
-		drive.goTo(reachTheFreightCalc, 0.16, 1, this.freightLocation.angle, 0.05, 0);
+		drive.goTo(reachTheFreightCalc, 0.12, 1, this.freightLocation.angle, 0.05, 0);
 		double timer = opMode.time;
 		while(opMode.opModeIsActive() && !handrail.freightIn() && (opMode.time < timer + 1.5));
 		if (!handrail.freightIn()) {
+			drive.drive(-0.02, 0, 1, this.freightLocation.angle, false);
+			drive.drive(0.05, 0, 1, this.freightLocation.angle, false);
+			while (opMode.opModeIsActive() && !handrail.freightIn() && (opMode.time < timer + 3.5));
+		}
+		if (!handrail.freightIn()) {
+			drive.drive(-0.02, 0, 1, this.freightLocation.angle, false);
 			drive.drive(0.05, 0, 1, this.freightLocation.angle, false);
 			while (opMode.opModeIsActive() && !handrail.freightIn()) {
-				if (opMode.time > timer + 4) {
+				if (opMode.time > timer + 8) {
 					return false;
 				}
 			}
