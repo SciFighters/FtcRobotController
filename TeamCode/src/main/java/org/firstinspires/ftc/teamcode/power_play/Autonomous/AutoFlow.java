0530f5bd720d0122d1e4083dbc4d502889152251
DanPeled <danpeled31@gmail.com>
2022-11-20 14:15:15 +0200

    Started working on AutoFlow.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/power_play/Autonomous/AutoFlow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/power_play/Autonomous/AutoFlow.java
@@ -1,8 +1,15 @@
 package org.firstinspires.ftc.teamcode.power_play.Autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.freight_frenzy.util.DriveClass;
+
 public class AutoFlow {
+    private LinearOpMode opMode = null;
+    private DriveClass drive = null;
+
+    final double tile = 0.6;
+
+
 
 }
