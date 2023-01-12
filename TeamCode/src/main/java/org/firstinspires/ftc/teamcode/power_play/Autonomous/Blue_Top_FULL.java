13eaf6771ec9569cb52c6fbf1d48866d22e7457a
Barak Mental <mental.barak@gmail.com>
2023-01-12 20:42:59 +0200

    * DriveClass.java: added hoverboard telemetry method and angle fixing method. * AutoFlow.java: changed alliance to left and right (top and bottom from upper POV) from XY-axis perspective.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/power_play/Autonomous/Blue_Top_FULL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/power_play/Autonomous/Blue_Top_FULL.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class Blue_Top_FULL extends LinearOpMode {
 	@Override
 	public void runOpMode() {
-		AutoFlow auto = new org.firstinspires.ftc.teamcode.power_play.Autonomous.AutoFlow(this, AutoFlow.ALLIANCE.BLUE, AutoFlow.StartPos.front, AutoFlow.Auto.FULL);
+		AutoFlow auto = new org.firstinspires.ftc.teamcode.power_play.Autonomous.AutoFlow(this, AutoFlow.StartPos.RIGHT , AutoFlow.Auto.FULL);
 
 		auto.init();
 
